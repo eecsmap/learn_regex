25c50180f01eacf172c5db00ba773686edcf1639
Wenyang Duan <wenyang.duan@gmail.com>
2021-02-23 16:27:11 -0800

    take out first_match

--- regex.c
+++ regex.c
@@ -1,9 +1,16 @@
 #include "regex.h"
+#include <assert.h>
+
+static inline bool first_match(char const *s, char const *p)
+{
+	assert(*p);
+	return *s && *p == '.' || *s == *p;
+}
 
 bool regex_match(char const *s, char const *p)
 {
 	if (!*p) return !*s;
-	if (p[1] == '*') return regex_match(s, p+2) || (*s == *p || *s && *p == '.') && regex_match(s+1, p);
-	if (*p == *s || *s && *p == '.') return regex_match(s+1, p+1);
+	if (p[1] == '*') return regex_match(s, p+2) || first_match(s, p) && regex_match(s+1, p);
+	if (first_match(s, p)) return regex_match(s+1, p+1);
 	return false;
 }
